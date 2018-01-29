5671c88be74bd1978eee1f03c87b6bcb00c6d2af
Chase Sawyer <sawych08@u.washington.edu>
2018-01-29 11:43:39 -0800

    first version of challenge 3 completed

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
-const mongoURL = process.env.MLABCONNECTION;
+const mongoURL = process.env.MONGODB_URI;
 const mdbName = process.env.MDBNAME;
+const shortUrlCollection = 'shortUrls';
 var app = express();
 
 
@@ -58,16 +59,39 @@ app.get("/shortener/new/:url", function (req, res) {
     original_url: "invalid URL",
     short_url: null
   };
+  resData.short_url = req.hostname + "/shortener/";
   /// db.find().sort( {index} ).limit( 1 )
   /// how to insert at last index position - not using inbuilt object ids.
+
   if (isValidUrl(req.params.url)) {
+    resData.original_url = req.params.url;
+
     var mdbClient = mongo.connect(mongoURL, function (err, client) {
       if (err) console.error(err);
       const db = mdbClient.db(mdbName);
-      db.collection('shortUrls').insertOne()
+      var lastDoc = db.collection(shortUrlCollection).find().sort({
+        index: -1
+      }).limit(1);
+      var insertIndex = 1;
+      if (lastDoc !== null) {
+        lastDoc.project({_id: 0, index: 1}).toArray(function (err, documents) {
+          if (err) console.error(err);
+          // console.log(documents[0].index);
+          insertIndex += documents[0].index;
+        });
+      }
+      db.collection(shortUrlCollection).insertOne({
+        index: insertIndex,
+        url: resData.original_url
+      }, function(err, r) {
+        if (err) console.error(err);
+        resData.short_url += insertIndex;
+      });
     });
     mdbClient.close();
-  };
+
+    res.json(resData);
+  }
 });
 
 function isValidURL(str) {
@@ -83,6 +107,27 @@ function isValidURL(str) {
     return true;
   }
 };
+
+app.get("/shortener/:id?", function(req, res) {
+  if (req.params.id !== null) {
+    var mdbClient = mongo.connect(mongoURL, function (err, client) {
+      if (err) console.error(err);
+      const db = mdbClient.db(mdbName);
+      var shortDestDoc = db.collection(shortUrlCollection).find({
+        index: parseInt(req.params.id)
+      }).project({
+        _id: 0,
+        url: 1
+      }).toArray(function (err, documents) {
+        if (err) console.error(err);
+        res.redirect(documents[0].url);
+      });
+    });
+    mdbClient.close();
+  } else {
+    res.end(JSON.stringify({"error":"invalid URL"}));
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
