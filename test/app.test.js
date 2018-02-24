1df3a5f2416fbed64ad594d9aff56eee622b3381
Chase Sawyer <sawych08@u.washington.edu>
2018-02-24 15:23:50 -0800

    Modified app to allow mocha test to wait for the server to properly get started before trying to start running the tests. Extended timeout to 60 seconds to allow for slow connections to resolve.

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,25 @@
+process.env.NODE_ENV = 'test'
+
 const chai = require('chai')
-const expect = require('chai').expect
+const should = chai.should()
 
 chai.use(require('chai-http'))
 
 const app = require('../app')
 
-describe('Main app functionality', function() {
-  //
+describe('Main app functionality', function () {
+  before(function (done) {
+    app.on('serverOK', function () {
+      done()
+    })
+  })
+  //test 404 page
+  describe('/404 error', function () {
+    it('should return an error for bad route', function(done) {
+      chai.request(app).get('/bad-link').end(function(err, res) {
+        res.should.have.status(404)
+        done()
+      })
+    })
+  })
 })
